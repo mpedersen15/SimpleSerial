a88e0e68dfe390ee010ac32906726ccdd655c1a5
mpedersen15 <mpedersen@mindrocketdesign.com>
2014-02-19 09:36:32 -0700

    Edit address

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@
 
 var app = {
     //macAddress: "9C:2A:70:0C:E4:30",  // get your mac address from bluetoothSerial.list
-    macAddress: "DF:6D:1F:1D:6A:B7",
+    macAddress: "",
 	chars: "",
 
 /*
